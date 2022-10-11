a9f5757f1c1968bbba779ab691514f76bfc5cc24
gerald glatt <geraldglatt89@gmail.com>
2022-10-11 14:23:56 +0200

    built first page

---
+++ src/Controller/TestController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController
+{
+    public function index() 
+    {
+        dd("Ca fonctionne!");
+        
+    }
+    /**
+     * @Route("/test/{age<\d+>?0}", name="test", methods={"GET", "POST"},
+     * host="127.0.0.1", schemes={"http", "https"})
+     */
+    public function test(Request $request, $age) 
+    {
+        return new Response("Vous avez $age ans !");
+    }
+}
