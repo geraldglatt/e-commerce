1160af259e603f08e5948b02bd40f658d92d0d50
gerald glatt <geraldglatt89@gmail.com>
2022-10-19 18:43:54 +0200

    create entity Category and add fixtures bundle

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -11,6 +12,7 @@ class HomeController extends AbstractController {
      * @Route("/", name="homepage")
      */
     public function homepage() {
+        
         return $this->render('home.html.twig');
     }
 
