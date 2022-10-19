9d8a67d09ad181338fae9c67a30582ff7dca13d2
gerald glatt <geraldglatt89@gmail.com>
2022-10-19 23:12:09 +0200

    Add fixtures,faker,mbezhanov & string composant

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,37 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use Faker\Factory;
+use App\Entity\Product;
+use Liior\Faker\Prices;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class AppFixtures extends Fixture
 {
+    protected $slugger;
+
+    public function __construct(sluggerInterface $slugger)
+    {
+        $this->slugger = $slugger;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+
+        $faker = Factory::create('fr_FR');
+        $faker->addProvider(new \Liior\Faker\Prices($faker));
+        $faker->addProvider(new \Bezhanov\Faker\Provider\Commerce($faker));
+
+        for($p = 0; $p < 100;$p++) {
+        $product = new Product;
+        $product->setName($faker->productName)
+                ->setPrice($faker->price(4000,20000))
+                ->setSlug(strtolower($this->slugger->slug($product->getName())));
+        
+        $manager->persist($product);
+       }
 
         $manager->flush();
     }
