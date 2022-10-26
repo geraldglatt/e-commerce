b144cc89152f69dbd814019b601d9950721dddcf
gerald glatt <geraldglatt89@gmail.com>
2022-10-26 19:03:24 +0200

    Add addEventListener in ProductType

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -11,6 +11,8 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ProductType extends AbstractType
@@ -39,14 +41,54 @@ class ProductType extends AbstractType
                     'attr' => ['placeholder' => 'Tapez une URL d\'image !']
                 ])
                 ->add('category', EntityType::class, [
-                    'label' => 'category',
+                    'label' => 'Catégorie',
                     'placeholder' => '-- Choisir une catégorie --',
                     'class' => Category::class,
                     // 'choice_label'=> 'name',
                     'choice_label' => function (Category $category) {
                         return strtoupper($category->getName());
-                        }
-                    ]);
+                    }
+            
+                ]);
+
+                $builder->addEventListener(FormEvents::POST_SUBMIT, function(FormEvent $event) {
+                    $product = $event->getData();
+
+                    if($product->getPrice() !== null) {
+                        $product->setPrice($product->getPrice() * 1);
+                    }
+                });
+            
+                $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+                    $form = $event->getForm();
+                    
+                    
+                    /** @var Product */
+                    $product = $event->getData();
+
+                    if ($product->getPrice() !== null) {
+                        $product->setPrice($product->getPrice() / 1);
+                    }
+
+                    // if($product->getId() === null) {
+
+                    //     $form->add('category', EntityType::class, [
+                    //         'label' => 'Catégorie',
+                    //         'placeholder' => '-- Choisir une catégorie --',
+                    //         'class' => Category::class,
+                    //         // 'choice_label'=> 'name',
+                    //         'choice_label' => function (Category $category) {
+                    //             return strtoupper($category->getName());
+                    //         }
+                    
+                    // ]);
+
+                // }
+
+            });
+            
+
+
     }
 
     public function configureOptions(OptionsResolver $resolver): void
